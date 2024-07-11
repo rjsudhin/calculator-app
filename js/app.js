889c7c35d736576d44c39807ab67847219357ee5
SudhinRaj <58643406+rjsudhin@users.noreply.github.com>
2024-07-11 18:32:58 +0530

    operating numbers is not only working a given length

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
+let previousValue = ''
+let currentValue = ''
+let operator = ''
+
+
 const previousScreen = document.querySelector('.previous-screen')
 const currentScreen = document.querySelector('.current-screen')
 
 const numbers = document.querySelectorAll('.number')
 numbers.forEach(number => {
-   number.addEventListener('click', () => {
-      operatingNumbers()
+   number.addEventListener('click', (e) => {
+      let num = e.target.textContent
+      operatingNumbers(num)
+      // showing the values in screen
+      currentScreen.textContent = currentValue
    })
 })
 
-function operatingNumbers() {
-   console.log('number clicks')
+function operatingNumbers(num) {
+   if (currentValue.length <= 9) {
+      currentValue += num
+   }
 }
